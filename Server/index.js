556cf18adb1fb0a7d2379248f2af057f6f9babfb
mukul314 <mukulsharma3779@gmail.com>
2023-08-02 11:31:56 +0530

    cors

--- Server/index.js
+++ Server/index.js
@@ -2,24 +2,31 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import AuthRoute from './Routes/AuthRoute.js'
-import UserRoute from './Routes/UserRoute.js'
-import PostRoute from './Routes/PostRoute.js'
-import cors from 'cors'
-import UploadRoute from './Routes/UploadRoute.js'
+import AuthRoute from "./Routes/AuthRoute.js";
+import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js";
+import cors from "cors";
+import UploadRoute from "./Routes/UploadRoute.js";
 
 // Routes
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://social-media-frontend-pi6v60xre-mukul314.vercel.app/",
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(express.static('public')); 
-app.use('/images', express.static('images'));
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
 
 dotenv.config();
 
@@ -42,7 +49,7 @@ app.use((err, req, res, next) => {
 });
 
 // Routes usage
-app.use('/auth', AuthRoute)
-app.use('/user', UserRoute)
-app.use('/post', PostRoute)
-app.use('/upload', UploadRoute)
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use("/post", PostRoute);
+app.use("/upload", UploadRoute);
